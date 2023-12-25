f52f8a0931228fd0596b2816be3a0ae764e96d03
Berke Turanlıoğlu <berke.turanlioglu@appcent.mobi>
2023-12-25 11:28:16 +0300

    Initial Commit

---
+++ ZoomInAndOutGraph/ZoomInAndOutGraphApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZoomInAndOutGraphApp.swift
+//  ZoomInAndOutGraph
+//
+//  Created by Berke Turanlioglu on 25.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct ZoomInAndOutGraphApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
